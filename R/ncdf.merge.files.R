277421f36f1526a7b8fb8d6fef11b88c62655422
jbuttlar <jbuttlar@865bcff6-fcb3-0410-8817-011d556edce7>
2012-06-19 14:51:45 +0000

    debugging rev184

--- R/ncdf.merge.files.R
+++ R/ncdf.merge.files.R
@@ -26,30 +26,33 @@ ncdf.merge.files <- function(
   }  
   
   if (!is.null(time.diff))
-    if( sum(!(date.start.in[-1] - date.end.in[-length(date.end.in)] <= time.diff)) > 0 )
-      stop('Time period missing!')
+    if( sum(!(date.start.in[-1] - date.end.in[-length(date.end.in)] <= time.diff)) > 0 ) {
+      paste('Time period missing!')
+      return('Time period missing!')
+    }  
   
   
-  
+  date.start.out <- date.start.in[1]
+  date.end.out <- date.end.in[length(date.end.in)]
   if (length(time.range.out) == 2) {
     files.delete <- c() 
     if (date.start.in[1] < time.range.out[1]) {
       file.start.new <- ncdf.cut.files(file.names[1], time.range.out =  c(time.range.out[1], date.end.in[1]),
                                        fun.start= fun.start, fun.end = fun.end, format = format )
       file.names[1]  <- file.start.new
-      files.delete <- c(files.delete, file.start.new) 
+      files.delete   <- c(files.delete, file.start.new)
+      date.start.out <- time.range.out[1]
     } 
     if (date.end.in[length(date.end.in)] > time.range.out[2]) {
       file.end.new <- ncdf.cut.files(file.names[length(file.names)],
                                      time.range.out =  c(date.start.in[length(date.start.in)], time.range.out[2]),
                                      fun.start= fun.start, fun.end = fun.end, format = format )
       file.names[length(file.names)]  <- file.end.new
-      files.delete <- c(files.delete, file.end.new) 
+      files.delete <- c(files.delete, file.end.new)
+      date.end.out <- time.range.out[2]
     } 
-  } else {
-    date.start.out <- date.start.in
-    date.end.out <- date.end.in
   }
+  
   system('export SKIP_SAME_TIME=1')
   ofile  <- do.call(name.change, list(sub(do.call(fun.end, list(file.names[1])),
                                           do.call(fun.end, list(file.names[length(file.names)])),
@@ -58,6 +61,6 @@ ncdf.merge.files <- function(
   if (length(files.delete) > 0 )
     for (file.t in files.delete)
       file.remove(file.t)
-  invisible(list(file.out = ofile, date.range = c(date.start.out, date.end.out))
+  invisible(list(file.out = ofile, date.range = c(date.start.out, date.end.out)))
 }
 
