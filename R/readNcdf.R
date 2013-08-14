62d374885ebfdb8035f64c1082d1ec6a0870a5bb
Jannis v. Buttlar <jbuttlar@bgc-jena.mpg.de>
2013-08-14 19:33:03 +0200

    now facilitates variable name transfer scheme

--- R/readNcdf.R
+++ R/readNcdf.R
@@ -1,15 +1,20 @@
 readNcdf <- function(
-    ##title<< fast reading of ncdf data
-    ##description<< Convenience funtcion to automatically read in data from a ncdf file
-    ##              without specifying variable names and opening file connections
-    file.name ##<< character string: name of the ncdf file file to read the data from- 
-) 
-##author<<
-## Jannis v. Buttlar, MPI BGC Jena, Germany, jbuttlar@bgc-jena.mpg.de
+  ##title<< fast reading of ncdf data
+  file.name ##<< character string: name of the ncdf file file to read the data from.
+  ,var.name = c()  ##<< character string: name of the variable to extract. If not supplied,
+            ## this is tried to be determined with readNcdfVarName().
+)
+  ##description<<
+  ## Convenience function to automatically read in data from a ncdf file
+  ## without specifying variable names and opening file connections.
+  ##author<<
+  ## Jannis v. Buttlar, MPI BGC Jena, Germany, jbuttlar@bgc-jena.mpg.de
 {
   require(RNetCDF)
   file.con <- open.nc(file.name)
-  data     <- var.get.nc(file.con, readNcdfVarName(file.con))
+  if (length(var.name) == 0)
+    var.name = readNcdfVarName(file.con)
+  data     <- var.get.nc(file.con, var.name)
   close.nc(file.con)
   ##value<< (multidimensional) array: data from the ncdf file.
   return(data)
