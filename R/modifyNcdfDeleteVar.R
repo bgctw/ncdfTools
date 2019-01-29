ff612afa49c217d4a69ab705a5ee90bf2bc30a7c
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2019-01-29 13:25:20 +0100

    intermediate in current dir

--- R/modifyNcdfDeleteVar.R
+++ R/modifyNcdfDeleteVar.R
@@ -3,11 +3,16 @@ modifyNcdfDeleteVar <- function(
   fileName     ##<< scalar string name of the file, that must not be open
   , varNames   ##<< string vector of variables to delete
   , isVerbose = FALSE ##<< set to TRUE to repeat CDO command
+  , intermediateFilename = paste0(fileName,"_ncdfTools",sample.int(1000,1)) ##<<
+  ## name of the intermediate file generated. 
 ) {
   ##details<< if varName is not found among variable names, does nothing
   if (!any(varNames %in% infoNcdfVars(fileName)$name)) return(TRUE)
-  # cdo writes to a temporary file, which then replaces the original
-  intermediate <- tempfile()
+  ##details<< cdo writes to a temporary file, which then replaces the original.
+  ## To avoid copying large files across boundaries of mounted file systems,
+  ## the intermediate file is created in the same directory as the original file 
+  ## by default.
+  intermediate <- intermediateFilename
   # intermediate <- "tmp.nc"
   cmd <- paste0(
     "cdo delname,",paste(varNames, collapse = ",")
